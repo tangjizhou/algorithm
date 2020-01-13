2dd25d0b61991b1cdc1aa5128b59f724a4905bc9
tangjizhou <tangjizhouchn@foxmail.com>
2020-01-13 22:22:45 +0800

    进制转换

--- src/main/java/net/mshome/twisted/algorithm/huawei/online/S0014/Main.java
+++ src/main/java/net/mshome/twisted/algorithm/huawei/online/S0014/Main.java
@@ -1,36 +1,70 @@
 package net.mshome.twisted.algorithm.huawei.online.S0014;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
- * 题目描述
- * 正整数A和正整数B 的最小公倍数是指 能被A和B整除的最小的正整数值，设计一个算法，求输入A和B的最小公倍数。
- * 输入描述:
- * 输入两个正整数A和B。
- * 输出描述:
- * 输出A和B的最小公倍数。
- * 示例1
- * 输入
- * <p>
- * 复制
- * 5 7
- * 输出
- * <p>
- * 复制
- * 35
- *
- * @author tangjizhouchn@foxmail.com
- * @date 2020/1/8
+ * 进制转换，将a-z --> 1-26
+ * 输入数字转换为字符串，输入字符串转换为数字
  */
 public class Main {
 
+    private static final Pattern CHAR_REGEXP = Pattern.compile("[a-z]+");
+    private static final Pattern NUMBER_REGEXP = Pattern.compile("[0-9]+");
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         while (scanner.hasNext()) {
-            // todo
+            String input = scanner.nextLine();
+            if (CHAR_REGEXP.matcher(input).matches()) {
+                System.out.println(convertStringToNumber(input));
+                continue;
+            }
+
+            if (NUMBER_REGEXP.matcher(input).matches()) {
+                System.out.println(convertNumberToString(input));
+                continue;
+            }
+            System.out.println("ERROR");
+
         }
     }
+
+
+    private static int convertStringToNumber(String input) {
+        char[] inputChars = input.toCharArray();
+        if (inputChars.length == 1) {
+            return convertTo26(inputChars[0]);
+        }
+
+        int result = 0;
+        double power = 0d;
+        for (int i = inputChars.length - 1; i >= 0; i--) {
+            result += Math.pow(26d, power++) * convertTo26(inputChars[i]);
+        }
+        return result;
+    }
+
+    private static String convertNumberToString(String input) {
+        int inputNumber = Integer.parseInt(input);
+        if (inputNumber <= convertTo26('z')) {
+            return String.valueOf((char) (inputNumber + 96));
+        }
+        StringBuilder result = new StringBuilder();
+        int remainder = 0;
+        while (inputNumber > 0) {
+            remainder = inputNumber % 26;
+            result.insert(0, (char) (remainder + 96));
+            inputNumber = (inputNumber - remainder) / 26;
+        }
+
+        return result.toString();
+    }
+
+    private static int convertTo26(char c) {
+        return c - 96;
+    }
+
 
 
 }
