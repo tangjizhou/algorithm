5335a8b13167b6b121da0de71767072eb00743be
tangjizhou <tangjizhouchn@foxmail.com>
2020-08-08 15:27:43 +0800

    栈实现队列

--- src/main/java/net/mshome/twisted/algorithm/leecode/MyQueue.java
+++ src/main/java/net/mshome/twisted/algorithm/leecode/MyQueue.java
@@ -11,42 +11,55 @@ import java.util.Stack;
  */
 public class MyQueue {
 
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
+    private Stack<Integer> pushStack;
+    private Stack<Integer> popStack;
 
     /**
      * Initialize your data structure here.
      */
     public MyQueue() {
-
+        pushStack = new Stack<>();
+        popStack = new Stack<>();
     }
 
     /**
      * Push element x to the back of queue.
      */
     public void push(int x) {
-
+        pushStack.push(x);
     }
 
     /**
      * Removes the element from in front of queue and returns that element.
      */
     public int pop() {
-
+        if (!popStack.isEmpty()) {
+            return popStack.pop();
+        }
+        while (!pushStack.isEmpty()) {
+            popStack.push(pushStack.pop());
+        }
+        return popStack.pop();
     }
 
     /**
      * Get the front element.
      */
     public int peek() {
-
+        if (!popStack.isEmpty()) {
+            return popStack.peek();
+        }
+        while (!pushStack.isEmpty()) {
+            popStack.push(pushStack.pop());
+        }
+        return popStack.peek();
     }
 
     /**
      * Returns whether the queue is empty.
      */
     public boolean empty() {
-
+        return pushStack.isEmpty() && popStack.isEmpty();
     }
 
 }
