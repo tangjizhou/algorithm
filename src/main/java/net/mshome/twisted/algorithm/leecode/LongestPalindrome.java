5704fa679f3e87a822cff93d5bfe4d5cc17bda98
tangjizhou <tangjizhouchn@foxmail.com>
2020-12-19 23:41:47 +0800

    动态规划最长回文子串

--- src/main/java/net/mshome/twisted/algorithm/leecode/LongestPalindrome.java
+++ src/main/java/net/mshome/twisted/algorithm/leecode/LongestPalindrome.java
@@ -17,25 +17,24 @@ public class LongestPalindrome {
     public void main() {
         Assert.assertEquals("cbbc", longestPalindrome("acbbcd"));
         Assert.assertEquals("acbbca", dpLongestPalindrome("acbbca"));
+        Assert.assertEquals("cbbc", dpLongestPalindrome("acbbcd"));
+        Assert.assertEquals("a", dpLongestPalindrome("ac"));
     }
 
     // 动态规划
     public String dpLongestPalindrome(String s) {
         char[] chars = s.toCharArray();
-        boolean[][] dp = new boolean[chars.length][chars.length];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i][i] = true;
+        if (chars.length < 2) {
+            return s;
         }
-
+        boolean[][] dp = new boolean[chars.length][chars.length];
         // dp[i][j] = dp[i+1][j-1] && s[i] = s[j]
         int max = 0, start = 0;
         for (int j = 0; j < chars.length; j++) {
-            for (int i = 0; i < j; i++) {
+            for (int i = 0; i <= j; i++) {
                 if (chars[i] != chars[j]) {
                     dp[i][j] = false;
-                    continue;
-                }
-                if (j - i < 3) {
+                } else if (j - i < 3) {
                     dp[i][j] = true;
                 } else {
                     dp[i][j] = dp[i + 1][j - 1];
@@ -69,13 +68,11 @@ public class LongestPalindrome {
 
     private int expandAroundCenter(char[] chars, int start, int end) {
         int maxLength = 0;
-        for (int i = 0; i < chars.length; i++) {
-            while (start >= 0 && end < chars.length && chars[start] == chars[end]) {
-                int length = end - start + 1;
-                maxLength = Math.max(length, maxLength);
-                --start;
-                ++end;
-            }
+        while (start >= 0 && end < chars.length && chars[start] == chars[end]) {
+            int length = end - start + 1;
+            maxLength = Math.max(length, maxLength);
+            --start;
+            ++end;
         }
         return maxLength;
     }
