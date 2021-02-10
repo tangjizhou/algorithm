5277f424ddba1eae5dd6a645432c970afceb18c0
tangjizhou <tangjizhouchn@foxmail.com>
2021-02-10 22:30:45 +0800

    100. 相同的树

---
+++ src/main/java/net/mshome/twisted/algorithm/leecode/SameTree.java
@@ -0,0 +1,93 @@
+package net.mshome.twisted.algorithm.leecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/same-tree/
+ *
+ * @author tangjizhouchn@foxmail.com
+ * @since 2021/1/27
+ */
+public class SameTree {
+
+    // Definition for a binary tree node.
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    @Test
+    public void test() {
+        TreeNode pRoot = new TreeNode(0);
+        pRoot.left = new TreeNode(1);
+        pRoot.right = new TreeNode(2);
+
+        TreeNode qRoot = new TreeNode(0);
+        qRoot.left = new TreeNode(1);
+        qRoot.right = new TreeNode(1);
+
+        Assert.assertTrue(isSameTreeBFS(pRoot, qRoot));
+    }
+
+
+    // DFS
+    public boolean isSameTreeDFS(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        return isSameTreeDFS(p.left, q.left) && isSameTreeDFS(p.right, q.right) && p.val == q.val;
+    }
+
+    // BFS
+    public boolean isSameTreeBFS(TreeNode p, TreeNode q) {
+        LinkedList<TreeNode> pNodes = new LinkedList<>();
+        LinkedList<TreeNode> qNodes = new LinkedList<>();
+        pNodes.offer(p);
+        qNodes.offer(q);
+
+        while (!pNodes.isEmpty() && !qNodes.isEmpty()) {
+            TreeNode pNode = pNodes.poll();
+            TreeNode qNode = qNodes.poll();
+            if (pNode == null || qNode == null) {
+                if (pNode == qNode) {
+                    continue;
+                }
+                return false;
+            }
+            if (pNode.left != null || qNode.left != null) {
+                pNodes.offer(pNode.left);
+                qNodes.offer(qNode.left);
+            }
+            if (pNode.right != null || qNode.right != null) {
+                qNodes.offer(qNode.right);
+                pNodes.offer(pNode.right);
+            }
+            if (pNode.val != qNode.val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
+
