d4c1838f904566c24012836b58dcdaac2400869b
tangjizhou <tangjizhouchn@foxmail.com>
2021-05-20 23:35:40 +0800

    二分查找

---
+++ src/main/java/net/mshome/twisted/algorithm/leecode/BinarySearch.java
@@ -0,0 +1,32 @@
+package net.mshome.twisted.algorithm.leecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * https://leetcode-cn.com/problems/binary-search/
+ * 二分查找
+ *
+ * @author tangjizhou
+ * @since 2021/5/19
+ */
+public class BinarySearch {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(0, search(new int[]{2, 5}, 2));
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) return middle;
+            else if (nums[middle] > target) right = middle - 1;
+            else if (nums[middle] < target) left = middle + 1;
+        }
+        return -1;
+    }
+
+}
