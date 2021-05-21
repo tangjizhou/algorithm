d7b6f201135eeb7fb2eed645ff53bd8dc79ccc9f
tangjizhou <tangjizhouchn@foxmail.com>
2021-05-22 00:07:56 +0800

    可信考试题目解答

---
+++ src/main/java/net/mshome/twisted/algorithm/huawei/online/SumUnmarkedTreeNode.java
@@ -0,0 +1,69 @@
+package net.mshome.twisted.algorithm.huawei.online;
+
+import net.mshome.twisted.algorithm.data.structure.tree.TreeNode;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 二叉树的一条根到叶子节点到路径中如果存在一个点将路径分为和相等的2段，则进行标记，返回所有未标记段点的和
+ *
+ * @author tangjizhou
+ * @since 2021/5/21
+ */
+public class SumUnmarkedTreeNode {
+
+
+    private final List<Integer> marked = new ArrayList<>();
+    private int total = 0;
+
+    @Test
+    public void test() {
+        TreeNode node = new TreeNode(7);
+        node.left = new TreeNode(6);
+        node.left.left = new TreeNode(3);
+        node.left.left.left = new TreeNode(11);
+        node.left.left.left.left = new TreeNode(2);
+        node.right = new TreeNode(5);
+        node.right.left = new TreeNode(4);
+        node.right.left.left = new TreeNode(1);
+        node.right.left.right = new TreeNode(3);
+        node.right.left.left.left = new TreeNode(16);
+        System.out.println(sum(node));
+        System.out.println();
+    }
+
+    public int sum(TreeNode root) {
+        sumHelper(root.left, root.val);
+        sumHelper(root.right, root.val);
+        int markedSum = marked.stream().mapToInt(v -> v).sum();
+        return total + root.val - markedSum;
+    }
+
+    public List<Integer> sumHelper(TreeNode node, int sum) {
+        if (node == null) {
+            return Collections.emptyList();
+        }
+        total += node.val;
+        List<Integer> sums = new ArrayList<>();
+        List<Integer> leftSums = sumHelper(node.left, sum + node.val);
+        List<Integer> rightSums = sumHelper(node.right, sum + node.val);
+        if (leftSums.isEmpty() && rightSums.isEmpty()) {
+            return List.of(node.val);
+        }
+
+        for (Integer leftSum : leftSums) {
+            if (leftSum == sum) marked.add(node.val);
+            sums.add(leftSum + node.val);
+        }
+        for (Integer rightSum : rightSums) {
+            if (rightSum == sum) marked.add(node.val);
+            sums.add(rightSum + node.val);
+        }
+        return sums;
+    }
+
+
+}
