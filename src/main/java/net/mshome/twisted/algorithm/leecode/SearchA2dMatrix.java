1f0520bb93f3ed794518dafb8924f821021b3a1f
tangjizhou <tangjizhouchn@foxmail.com>
2021-09-19 14:48:35 +0800

    搜索二分矩阵

---
+++ src/main/java/net/mshome/twisted/algorithm/leecode/SearchA2dMatrix.java
@@ -0,0 +1,86 @@
+package net.mshome.twisted.algorithm.leecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * https://leetcode-cn.com/problems/search-a-2d-matrix/
+ * 搜索二维矩阵
+ *
+ * @author tangjizhou
+ * @since 2021/9/18
+ */
+public class SearchA2dMatrix {
+
+    @Test
+    public void test() {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60},
+                {70, 80, 90, 100}
+        };
+        Assert.assertFalse(searchMatrix1(matrix, 99));
+        Assert.assertTrue(searchMatrix1(matrix, 80));
+    }
+
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        int row = searchRow(matrix, target);
+        if (row == -1) {
+            return false;
+        }
+        return searchCol(matrix[row], target) != -1;
+    }
+
+    // 查询最后一个不大于目标值的行
+    private int searchRow(int[][] matrix, int target) {
+        int start = 0, end = matrix.length - 1;
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            if (middle == end || matrix[middle][0] == target) return middle;
+            if (matrix[middle][0] < target) {
+                if (matrix[middle + 1][0] > target) return middle;
+                else start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int searchCol(int[] row, int target) {
+        int start = 0, end = row.length - 1;
+        while (start <= end) {
+            int middle = start + ((end - start) >> 1);
+            if (row[middle] == target) {
+                return middle;
+            } else if (row[middle] < target) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int start = 0, end = m * n - 1;
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            int row = middle / n;
+            int col = middle % n;
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return false;
+    }
+
+}
